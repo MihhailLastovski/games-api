0feac2c99eb5121e2b6e1f6652b4d2db4ab7aecf
Opilane TTHK <opilane@tthk.ee>
2023-10-13 11:23:15 +0300

    As an app developer I want to get a full details of a given game to show in my app [Delivers #186246636]

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const app = require('express')()
-const port = 8080
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./docs/swagger.json');
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const yamljs = require('yamljs');
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+
+const app = express();
+const port = 8080;
+
+const games = [
+    {id: 1, name: "Dota 2", price: "life"},
+    {id: 2, name: "Witcher 3", price: 59.99},
+    {id: 3, name: "Cyberpunk 2077", price: 59.99},
+    {id: 3, name: "CS2", price: 0},
+    {id: 3, name: "Minecraft", price: 26.99},
+    {id: 3, name: "Forza Horizon 5", price: 59.99},
+    {id: 3, name: "Defense of the Ancients: Allstars", price: 0}
+]
 
 app.get('/games', (req, res) => {
-    res.send(["Witcher 3", "Cyberpunk 2077", "Dota 2"])
+    res.send(games)
+})
+
+app.get('/games/:id', (req, res) => {
+    if (typeof games[req.params.id - 1] === 'undefined'){
+        return res.status(404).send({error: "Game not found"})
+    }
+    res.send(games[req.params.id - 1])
 })
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))

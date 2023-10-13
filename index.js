fab5afff2edf96309e969097a7d8d87a4d06cd81
Opilane TTHK <opilane@tthk.ee>
2023-10-13 11:59:17 +0300

    As a developer want to be able to add new games [Delivers #186246666]

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const yamljs = require('yamljs');
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 
 const app = express();
+app.use(express.json()); 
 const port = 8080;
 
 const games = [
     {id: 1, name: "Dota 2", price: "life"},
     {id: 2, name: "Witcher 3", price: 59.99},
     {id: 3, name: "Cyberpunk 2077", price: 59.99},
-    {id: 3, name: "CS2", price: 0},
-    {id: 3, name: "Minecraft", price: 26.99},
-    {id: 3, name: "Forza Horizon 5", price: 59.99},
-    {id: 3, name: "Defense of the Ancients: Allstars", price: 0}
+    {id: 4, name: "CS2", price: 0},
+    {id: 5, name: "Minecraft", price: 26.99},
+    {id: 6, name: "Forza Horizon 5", price: 59.99},
+    {id: 7, name: "Defense of the Ancients: Allstars", price: 0}
 ]
 
 app.get('/games', (req, res) => {
@@ -27,8 +28,29 @@ app.get('/games/:id', (req, res) => {
     res.send(games[req.params.id - 1])
 })
 
+app.post('/games', (req, res) => {
+    if (!req.body.name || !req.body.price){
+        return res.status(400).send({ error: 'One or all params are missing'})
+    }
+    let game = {
+        id: games.length + 1,
+        price: req.body.price,
+        name: req.body.name
+    }
+
+    games.push(game)
+
+    res.status(201).location(`${getBaseUrl(req)}/games/${games.length}`)
+    .send(game)
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http//localhost:${port}`)
 })
+
+function getBaseUrl(req){
+    return req.connection && req.connection.encrypted 
+    ? 'https' : 'http' + `://${req.headers.host}` 
+}
